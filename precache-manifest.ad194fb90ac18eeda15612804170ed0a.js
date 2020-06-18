39882382e347073ae92a4cf89178ed15c2681144
rananda dian ilham <rananda.d.ilham@gmail.com>
2020-06-18 09:47:17 +0700

    Updates

---
+++ precache-manifest.ad194fb90ac18eeda15612804170ed0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6400af4f9eb59ed8db850117f993ad",
+    "url": "/robosearch/index.html"
+  },
+  {
+    "revision": "60be8e04dba9c7da5c98",
+    "url": "/robosearch/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7a8bb828301025bebf62",
+    "url": "/robosearch/static/css/main.2f163592.chunk.css"
+  },
+  {
+    "revision": "60be8e04dba9c7da5c98",
+    "url": "/robosearch/static/js/2.8fff7d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosearch/static/js/2.8fff7d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8bb828301025bebf62",
+    "url": "/robosearch/static/js/main.a68f5ee4.chunk.js"
+  },
+  {
+    "revision": "699f8b80792de37fcda4",
+    "url": "/robosearch/static/js/runtime-main.789298e9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robosearch/static/media/SEGA.12a3db8b.woff"
+  }
+]);
